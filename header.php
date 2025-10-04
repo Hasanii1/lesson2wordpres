a7ec5bc946d7a066efbc81da57509b81ffeb46fc
Hasanii1 <ssenadhasani@gmail.com>
2025-10-04 12:49:31 +0200

    .......

--- header.php
+++ header.php
@@ -15,6 +15,15 @@
   <?php if ( get_bloginfo('description') ) : ?>
     <p class="site-tagline"><?php bloginfo('description'); ?></p>
   <?php endif; ?>
-
-
+  <nav class='site-nav'>
+    <?php   
+    wp_nav_menu(array(
+      'theme-location'=>'primary',
+      'menu_class'=>'main-menu'
+    ));
+    
+    
+    ?>
+  </nav>
   </header>
+  
